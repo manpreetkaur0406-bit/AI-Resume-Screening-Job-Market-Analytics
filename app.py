5e706e46433a1955d3404b7e923889dec07efcc0
manpreetkaur0406-bit <mangndec@gmail.com>
2026-07-25 17:59:26 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="AI Resume & Job Market Analytics",
+    page_icon="🤖",
+    layout="wide"
+)
+
+st.title("🤖 AI Resume & Job Market Analytics")
+
+st.write("Welcome to my Final Year Data Analytics Project!")
+
+st.markdown("""
+### Features
+- 📊 EDA
+- 📄 ATS Score
+- 🧠 Skill Gap Analysis
+- 💼 Job Recommendation
+- 💰 Salary Prediction
+- 📈 Salary Analytics
+
+👈 Choose a page from the sidebar.
+""")
